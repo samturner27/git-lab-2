cf4889028e12ece79ca34c5f6e99e190876e9f0e
Sam Turner <st976817@ohio.edu>
2022-01-18 10:26:59 -0500

    Committing program to git

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: <Sam Turner>
+ *        Date: <01/18/22>
+ * Description: Question 15 of git-lab
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
